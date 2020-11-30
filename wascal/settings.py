24f1aa2c31d34ed33078b104e3c83434a44050de
Eduildo Lima <limaeduildo@hotmail.com>
2020-11-30 03:30:45 +0000

    Changed

--- wascal/settings.py
+++ wascal/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-import django_heroku
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -144,4 +142,3 @@ LOGOUT_REDIRECT_URL = "/"
 MEDIA_URL = 'documents/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'documents')
 
-django_heroku.settings(locals())
